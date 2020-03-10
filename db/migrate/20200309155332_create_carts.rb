cc160cdde61369d0e72a9a117e5808f437644f2f
stephen <toulouse.stephen01@gmail.com>
2020-03-10 12:44:07 +0100

    bug

--- db/migrate/20200309155332_create_carts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCarts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :carts do |t|
-      t.belongs_to :user, :item, optional: true
-
-      t.timestamps
-    end
-  end
-end
