748cc6e97513aa503606c89d094d8ba3f6fc7aaa
AdrienCastelbou <acastelbou1@gmail.com>
2020-03-11 16:49:05 +0100

    admin added

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,12 @@
 require 'faker'
 
 Item.delete_all
+User.delete_all
+Cart.delete_all
+ItemCart.delete_all
 20.times do 
   file = open("http://placekitten.com/200/#{rand(200..300)}")
   Item.create(title: Faker::Creature::Cat.name, description: Faker::Creature::Cat.breed, price: rand(1..50)).cat_image.attach(io: file, filename: 'some-image.jpg')
-  
 end
+
+User.create(email: "admin@admin.com", name: "admin", password: "aadmin", admin: true)
