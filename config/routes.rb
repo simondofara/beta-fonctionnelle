9452d0d551fafc430802ff24b612b899a93d6490
Mikaël <rocavel.dev@zoho.eu>
2020-03-11 11:21:16 +0100

    corrections des routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,6 @@ Rails.application.routes.draw do
   }
   root 'items#index'
 
-  get 'carts/:id' => "carts#show", as: "cart"
-  delete 'carts/:id' => "carts#destroy"
-
-  post 'item_carts/:id/add' => "item_carts#add_quantity", as: "item_cart_add"
-  post 'item_carts/:id/reduce' => "item_carts#reduce_quantity", as: "item_cart_reduce"
-  post 'item_carts' => "item_carts#create"
-  get 'item_carts/:id' => "item_carts#show", as: "item_cart"
-  delete 'item_carts/:id' => "item_carts#destroy"
-
-
 
   resources :orders
 
