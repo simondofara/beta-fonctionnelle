ff69a2743f0cbf7c15b674a62e269c6e5ea91969
stephen <toulouse.stephen01@gmail.com>
2020-03-12 21:24:32 +0900

    add_item_to_cart last update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def cart_count_over_one
+   return total_cart_items if total_cart_items > 0
+ end
+
+ def total_cart_items
+   total = @cart.item_carts.map(&:quantity).sum
+   return total if total > 0
+ end
 end
